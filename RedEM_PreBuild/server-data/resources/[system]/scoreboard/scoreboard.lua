ada6a9398e4f15d5862fe563f58e92b5965fdf3a
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-05-26 17:29:07 -0700

    Add files via upload

---
+++ RedEM_PreBuild/server-data/resources/[system]/scoreboard/scoreboard.lua
@@ -0,0 +1,48 @@
+local listOn = false
+
+Citizen.CreateThread(function()
+    listOn = false
+    while true do
+        Wait(0)
+
+        if IsControlPressed(0, 0x80F28E95)--[[ INPUT_PHONE ]] then
+            if not listOn then
+                local players = {}
+                ptable = GetPlayers()
+                for _, i in ipairs(ptable) do
+                    local wantedLevel = GetPlayerWantedLevel(i)
+                    r, g, b = UsePlayerColourInsteadOfTeamColour(i)
+                    table.insert(players, 
+                    '<tr style="color: rgb(255, 255, 255); font-weight: 500;"><td>' .. GetPlayerServerId(i) .. '</td><td>' ..GetPlayerName(i)..'</td></tr>'
+                    )
+                end
+                
+                SendNUIMessage({ text = table.concat(players) })
+
+                listOn = true
+                while listOn do
+                    Wait(0)
+                    if(IsControlPressed(0, 0x80F28E95) == false) then
+                        listOn = false
+                        SendNUIMessage({
+                            meta = 'close'
+                        })
+                        break
+                    end
+                end
+            end
+        end
+    end
+end)
+
+function GetPlayers()
+    local players = {}
+
+    for i = 0, 31 do
+        if NetworkIsPlayerActive(i) then
+            table.insert(players, i)
+        end
+    end
+
+    return players
+end
