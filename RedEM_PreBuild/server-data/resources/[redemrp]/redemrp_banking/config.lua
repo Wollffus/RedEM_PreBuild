98c4c871af6f4c629d555faf6664686e179b7cf2
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-06-19 17:32:50 -0700

    Add files via upload

---
+++ RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_banking/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+Config.Shoptext = 'Press [ENTER] to access the Bank' -- Text to open the clothing shop
+Config.BlipName = 'Bank' -- Blip Name Showed on map
+Config.EnableCommand = true -- Enable/Disable /clothing command
+Config.BlipSprite = 249721687 -- Clothing shop sprite
+Config.BlipScale = 0.2 -- Blip scale
+Config.OpenKey = 0xC7B5340A -- Opening key hash
+Config.Zones = {
+	vector3(-308.57,775.95,117.7), -- VALENTINE
+	vector3(1292.48,-1301.67,77.04), -- RHODES
+	vector3(2644.23,-1292.61,52.25), -- SAINT DENIS
+	vector3(-812.18,-1277.26,43.64), -- BLACK WATER
+
+}
