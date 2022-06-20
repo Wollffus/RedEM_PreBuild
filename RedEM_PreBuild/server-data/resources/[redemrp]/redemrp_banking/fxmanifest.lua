00726899265717c877570ffe3c1f228fe517b59b
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-06-19 17:33:30 -0700

    Update fxmanifest.lua

--- RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_banking/fxmanifest.lua
+++ RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_banking/fxmanifest.lua
@@ -1,21 +1,25 @@
-client_script('client/client.lua')
+fx_version 'bodacious'
+
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+client_script{
+'client/client.lua',
+'config.lua'
+}
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
     'server.lua'
 }
 
-ui_page('client/html/UI.html') 
+ui_page('client/html/UI.html')
 
 
 files {
-	'client/html/UI.html', 
+	'client/html/UI.html',
     'client/html/style.css',
     'client/html/media/font/crock.ttf',
     'client/html/media/img/bg.png',
 	'client/html/media/img/bg2.png',
 }
-game 'rdr3'
-
-fx_version 'adamant'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
