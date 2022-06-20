9e9ada6c2b684b5962bc7417e8fd872c3c9d5d32
Wollffus <58875783+Wollffus@users.noreply.github.com>
2022-06-19 17:30:24 -0700

    Update client.lua

--- RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_banking/client/client.lua
+++ RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_banking/client/client.lua
@@ -1,13 +1,8 @@
-RegisterCommand('bank', function(source)
 
-			inMenu = true
-			SetNuiFocus(true, true)
-			SendNUIMessage({type = 'openGeneral'})
-			TriggerServerEvent('redemrp_banking:balance2')
-end)
 --=================Deposit Event===================
-local l_ 
-local f_ 
+local l_
+local f_
+local active = false
 RegisterNetEvent('currentbalance1')
 AddEventHandler('currentbalance1', function(balance, namel, namef)
 if namel ~= nil then
@@ -34,15 +29,10 @@ end)
 
 RegisterNUICallback('withdrawl', function(data)
 	TriggerServerEvent('redemrp_banking:withdraw', tonumber(data.amountw))
-	
 end)
 
 RegisterNUICallback('transfer1', function(data)
-print("poszlo")
-print(data.firstname)
-print(data.lastname)
-	TriggerServerEvent('redemrp_banking:transfer1', tonumber(data.amountw) , data.firstname, data.lastname)
-	
+ TriggerEvent('redemrp_inventory:OpenPrivateLocker')
 end)
 --======================Balance Event======================
 
@@ -50,11 +40,74 @@ RegisterNUICallback('balance', function()
 	TriggerServerEvent('redemrp_banking:balance')
 end)
 
-
---======================NUIFocusoff======================
-
 RegisterNUICallback('NUIFocusOff', function()
 	inMenu = false
 	SetNuiFocus(false, false)
 	SendNUIMessage({type = 'closeAll'})
+	active = false
 end)
+--======================NUIFocusoff======================
+
+
+
+Citizen.CreateThread(function()
+	Wait(0)
+	for k,v in pairs(Config.Zones) do
+		local blip = N_0x554d9d53f696d002(1664425300, v)
+		SetBlipSprite(blip, Config.BlipSprite, 1)
+		SetBlipScale(blip, Config.BlipScale)
+		Citizen.InvokeNative(0x9CB1A1623062F402, blip, Config.BlipName)
+	end
+end)
+
+local PromptBank
+local OpenBankGroup = GetRandomIntInRange(0, 0xffffff)
+--print('OpenBankGroup: ' .. OpenBankGroup)
+
+function OpenBank()
+    Citizen.CreateThread(function()
+        local str = "Bank"
+        PromptBank = PromptRegisterBegin()
+        PromptSetControlAction(PromptBank, 0xC7B5340A)
+        str = CreateVarString(10, 'LITERAL_STRING', str)
+        PromptSetText(PromptBank, str)
+        PromptSetEnabled(PromptBank, true)
+        PromptSetVisible(PromptBank, true)
+        PromptSetHoldMode(PromptBank, true)
+        PromptSetGroup(PromptBank, OpenBankGroup)
+        PromptRegisterEnd(PromptBank)
+
+    end)
+end
+Citizen.CreateThread(function()
+Wait(2000)
+ OpenBank()
+	while true do
+	Wait(0)
+		local playerPed = PlayerPedId()
+		local coords = GetEntityCoords(playerPed)
+	for k,v in pairs(Config.Zones) do
+			if Vdist(coords, v) < 1.5 and not active then
+				local BankGroupName  = CreateVarString(10, 'LITERAL_STRING', "Bank")
+				 PromptSetActiveGroupThisFrame(OpenBankGroup, BankGroupName)
+				if PromptHasHoldModeCompleted(PromptBank) then
+					inMenu = true
+					SetNuiFocus(true, true)
+					SendNUIMessage({type = 'openGeneral'})
+					TriggerServerEvent('redemrp_banking:balance2')
+					active = true
+				end
+			end
+		end
+	end
+end)
+
+function DrawTxt(str, x, y, w, h, enableShadow, col1, col2, col3, a, centre)
+    local str = CreateVarString(10, "LITERAL_STRING", str, Citizen.ResultAsLong())
+    SetTextScale(w, h)
+    SetTextColor(math.floor(col1), math.floor(col2), math.floor(col3), math.floor(a))
+    SetTextCentre(centre)
+    if enableShadow then SetTextDropshadow(1, 0, 0, 0, 255) end
+	Citizen.InvokeNative(0xADA9255D, 10);
+	DisplayText(str, x, y)
+end
