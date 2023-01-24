149498b34057ec6481f75b0348945d9b604861aa
Wollffus <58875783+Wollffus@users.noreply.github.com>
2023-01-24 14:57:45 -0800

    Add files via upload

---
+++ RedEM_PreBuild/server-data/resources/redemrp_status/status.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `status` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`identifier` VARCHAR(50) NOT NULL DEFAULT '0',
+	`charid` INT(11) NOT NULL DEFAULT 0,
+	`status` VARCHAR(255) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=100
+;
