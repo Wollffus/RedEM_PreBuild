ddd8ee1ad8c4d3cebc9e291932356bfd57710dba
Wollffus <58875783+Wollffus@users.noreply.github.com>
2023-01-24 22:33:53 -0800

    Delete RedEM_PreBuild/server-data/resources/WH_Whitelist directory

--- RedEM_PreBuild/server-data/resources/WH_Whitelist/client.lua
+++
@@ -1,8 +0,0 @@
-triggered = false;
-AddEventHandler("playerSpawned", function()
-    if not triggered then 
-        triggered = true;
-        Citizen.Wait((1000 * 20)); -- Wait 20 seconds
-        TriggerServerEvent('WH_Whitelist:PlayerLoaded');
-    end
-end)
