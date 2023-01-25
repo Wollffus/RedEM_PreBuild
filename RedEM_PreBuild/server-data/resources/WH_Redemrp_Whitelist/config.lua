f6b11de3e64d52037e3a6cb7315da2f7168a0485
Wollffus <58875783+Wollffus@users.noreply.github.com>
2023-01-24 22:34:07 -0800

    Add files via upload

---
+++ RedEM_PreBuild/server-data/resources/WH_Redemrp_Whitelist/config.lua
@@ -0,0 +1,19 @@
+Config = {
+	Guild_ID = 'CHANGE_ME',
+	Bot_Token = 'CHANGE_ME',
+	RoleList = {
+		['Whitelisted'] = CHANGE_ME,
+		['Staff'] = CHANGE_ME,
+		['Dev'] = CHANGE_ME,
+	},
+}
+
+Config.Splash = {
+	Header_IMG = 'https://i.imgur.com/ki78mGc.png',
+	Enabled = true,
+	Wait = 15,
+	Heading1 = "WolfHouse Studios RedEM:RP Reboot Pre-Build",
+	Heading2 = "Robert Banks was here....",
+	Discord_Link = 'https://discord.gg/zGVDppYxfk',
+	Website_Link = 'https://discord.gg/zGVDppYxfk',
+}
