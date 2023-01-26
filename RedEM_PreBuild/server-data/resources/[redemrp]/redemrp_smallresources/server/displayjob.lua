4e6458e66048f5c958e3ed970fb616c2013f7891
Wollffus <58875783+Wollffus@users.noreply.github.com>
2023-01-25 21:44:38 -0800

    Delete RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_smallresources/server directory

--- RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_smallresources/server/displayjob.lua
+++
@@ -1,7 +0,0 @@
-RegisterCommand('showjob', function(source)
-    local _source = source
-    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-        local job = user.getJob()
-        TriggerClientEvent('redem_roleplay:Tip', _source, job, 3000)
-    end)
-end)
--- RedEM_PreBuild/server-data/resources/[redemrp]/redemrp_smallresources/server/handcuffs.lua
+++
@@ -1,4 +0,0 @@
-RegisterServerEvent("RegisterUsableItem:handcuffs")
-AddEventHandler("RegisterUsableItem:handcuffs", function(source)
-    TriggerClientEvent('redemrp_smallresources:handcuffItem', source)
-end)
